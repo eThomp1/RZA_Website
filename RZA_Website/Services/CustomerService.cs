ee38ad0b7670ec1a48360cf4a1c433df7c070b23
Edward Thompson <S2303064@ndonline.ac.uk>
2024-11-04 12:30:42 +0000

    added Login and it kinda works

--- RZA_Website/Services/CustomerService.cs
+++ RZA_Website/Services/CustomerService.cs
@@ -9,5 +9,16 @@ namespace RZA_Website.Services
         {
             _context = context;
         }
+        public async Task AddCustomerAsync(Customer customer)
+        {
+            await _context.Customers.AddAsync(customer);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<Customer?> LogIn(Customer customer)
+        {
+            return await _context.Customers.FirstOrDefaultAsync(
+                c => c.Username == customer.Username &&
+                c.Password == customer.Password);
+        }
     }
 }

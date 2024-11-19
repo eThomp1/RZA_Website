88e1b7b9202758e0e802a6ebe9d3da7dd670a1b9
Edward Thompson <S2303064@ndonline.ac.uk>
2024-11-19 09:45:56 +0000

    Added Start of Ticket booking functionality

---
+++ RZA_Website/Services/TicketBookingService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RZA_Website.Models;
+
+namespace RZA_Website.Services
+{
+    public class TicketBookingService
+    {
+        private readonly TlS2303064Rza2Context _context;
+        public TicketBookingService(TlS2303064Rza2Context context)
+        {
+            _context = context;
+        }
+        public async Task<List<Ticketbooking>> GetTicketbookingsAsync()
+        {
+            return await _context.Ticketbookings.ToListAsync();
+        }
+        public async Task AddTicketbookingAsync(Ticketbooking newTicketbooking)
+        {
+            await _context.Ticketbookings.AddAsync(newTicketbooking);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
+
+
---
+++ RZA_Website/Services/TicketService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RZA_Website.Models;
+
+namespace RZA_Website.Services
+{
+   
+    public class TicketService
+    {
+        private readonly TlS2303064Rza2Context _context;
+        public TicketService(TlS2303064Rza2Context context)
+        {
+            _context = context;
+        }
+        public async Task<List<Ticket>> GetTicketsAsync()
+        {
+            return await _context.Tickets.ToListAsync();
+        }
+        public async Task AddTicketAsync(Ticket newTicket)
+        {
+            await _context.Tickets.AddAsync(newTicket);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
